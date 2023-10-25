97132a4b85b78689c4fa6e8a1cdce863c67125ea
Abil Manoj <axm1926@student.bham.ac.uk>
2023-10-25 15:17:32 +0100

    about section completed and button to add resume also added to NavBar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+
 const Hero = () => {
+  const scrollToSection = (e) => {
+    e.preventDefault();
+    const targetSection = document.getElementById("about");
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -24,21 +33,21 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+      <a href="#about" onClick={scrollToSection}>
+        <div className="relative xs:bottom-10 bottom-32 w-full flex justify-center items-center cursor-pointer">
+          <div className="w-[30px] h-[54px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
-              animate={{ y: [0, 24, 0] }}
+              animate={{ y: [0, 22, 0] }}
               transition={{
-                duration: 1.5,
+                duration: 2,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-5 h-2 rounded-full bg-secondary mb-1"
             />
           </div>
-        </a>
-      </div>
+        </div>{" "}
+      </a>
     </section>
   );
 };
